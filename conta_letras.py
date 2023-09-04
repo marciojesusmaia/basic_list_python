e14703d4bb55239b97ca455b37bff8aa2b70a094
Marcio Maia <88562590+marciojesusmaia@users.noreply.github.com>
2023-09-04 13:24:53 -0300

    explicação do codigo

--- conta_letras.py
+++ conta_letras.py
@@ -1,3 +1,9 @@
+'''
+No exemplo, a variavel c recebe uma palavra ou frase.
+Logo abaixo, usando o metodo `join` e `split` juntamos tudo e retiramos os espaços.
+Em seguida com o metodo `len` contamos quantas letras possuem na palavra ou frase digitada
+'''
+
 c = input('Digite uma palavra/frase para saber quantas letras tem: ')
 c = ''.join(c.split())
 print('Sua palavra/frase tem ',len(c),' letras.')
