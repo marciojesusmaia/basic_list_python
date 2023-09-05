9f2160f4b04bbbe146c4c546b49c1abfb58948db
Marcio Maia <88562590+marciojesusmaia@users.noreply.github.com>
2023-09-05 07:31:42 -0300

    renomeado variaveis

--- add_list.py
+++ add_list.py
@@ -1,13 +1,14 @@
 '''Para adicionar itens em uma lista usamos o metodo `append`
 No exemplo, enquanto a variavel for menor que 6, constinuara sendo adicionados itens na lista
-em uma contagem crescente.'''
+em uma contagem crescente.
+Ao final é mostrado a lista atualizada.'''
 
 
-l2 = []
-a=0
-while a<6:
-  i=input('Digite 6 itens para adicionar a lista ')
-  a=a+1
-  l2.append(i)
+lista = []
+contador = 0
+while contador < 6:
+  itens = input('Digite 6 itens para adicionar a lista ')
+  contador = contador + 1
+  lista.append(itens)
 
-print('A sua lista é: ', l2)
+print('A sua lista é: ', lista)
