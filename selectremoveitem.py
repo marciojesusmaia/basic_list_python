e01a1e90d4723a3043db30ed1dd60eadd6e26b53
Marcio Maia <88562590+marciojesusmaia@users.noreply.github.com>
2023-09-05 07:19:44 -0300

    explicação+renomeio

--- selectremoveitem.py
+++ selectremoveitem.py
@@ -1,5 +1,12 @@
-l4 = ['linux', 'solaris', 'windows', 'unix', 'android', 'ios', 'chromeOs']
-print(l4)
-e = input('Qual item deseja remover da lista? ')
-l4.remove(e)
-print(l4)
+'''
+Para remover um item em especifico usamos o metodo `remove`.
+No exemplo abaixo, podemos escolher qual o item da lista desejamos escolher,
+e ao final exibimos o resultado sem o item que foi escolhido para retirada.
+'''
+
+
+sistemas = ['linux', 'solaris', 'windows', 'unix', 'android', 'ios', 'chromeOs']
+print(sistemas)
+escolha = input('Qual item deseja remover da lista? ')
+sistemas.remove(escolha)
+print(sistemas)
